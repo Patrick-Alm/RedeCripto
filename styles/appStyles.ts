163034454db00868055c3e629ce2a5aa2ffe5dea
Pedro Pereira <devpereirapedro@gmail.com>
2025-04-30 20:05:25 -0300

    style: add title style

--- styles/appStyles.ts
+++ styles/appStyles.ts
@@ -1,23 +1,29 @@
-import { theme } from '@/constants/theme';
-import { StyleSheet } from 'react-native';
+import { theme } from "@/constants/theme";
+import { StyleSheet } from "react-native";
 
 export const appStyles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: theme.colors.background,
-	},
-	content: {
-		padding: theme.spacing.lg,
-	},
-	header: {
-		marginBottom: theme.spacing.lg,
-	},
-	greeting: {
-		fontSize: theme.fontSizes.lg,
-		color: theme.colors.text,
-		fontWeight: '600',
-	},
-	subtitle: {
-		color: theme.colors.textMuted,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    padding: theme.spacing.md,
+  },
+  content: {
+    padding: theme.spacing.lg,
+  },
+  header: {
+    marginBottom: theme.spacing.lg,
+  },
+  greeting: {
+    fontSize: theme.fontSizes.lg,
+    color: theme.colors.text,
+    fontWeight: "600",
+  },
+  subtitle: {
+    color: theme.colors.textMuted,
+  },
+  title: {
+    fontSize: theme.fontSizes.xl,
+    fontWeight: "bold",
+    color: theme.colors.text,
+  },
 });
