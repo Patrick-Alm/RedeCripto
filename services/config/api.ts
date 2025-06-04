91619b0af648388ade71dd8b23e4e4416c6724d5
Pedro Pereira <devpereirapedro@gmail.com>
2025-06-04 20:51:41 -0300

    fix: env name

--- services/config/api.ts
+++ services/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_URL || "http://192.168.8.185:3001";
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://192.168.8.185:3001";
 
 const api = axios.create({
 	baseURL: BASE_URL,
